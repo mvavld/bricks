30ffcf9ff4cb05d387e1c571e4c8b98eb3c17baf
Vlad Melnikov <86787868+mvavld@users.noreply.github.com>
2025-04-22 09:15:43 +0300

    Рефактор

--- bricks/repository/__brick__/lib/data/data_sources/{{name.snakeCase()}}_data_source.dart
+++ bricks/repository/__brick__/lib/data/data_sources/{{name.snakeCase()}}_data_source.dart
@@ -1,5 +1,5 @@
 import '../network/responses/rm_server_response.dart';
 
 abstract class {{name.pascalCase()}}DataSource {
-Future<RmServerResponse> {{method.camelCase()}}();
+  Future<RmServerResponse> {{method.camelCase()}}();
 }
